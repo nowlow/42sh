1ccee471240f784282adc42b4a229d3e5665df54
axo <axo@localhost.localdomain>
2019-05-21 10:16:28 +0200

    [FIX] coding style pipes

--- pip/matteo.c
+++ pip/matteo.c
@@ -15,26 +15,28 @@
 #define TUBE_ECRITURE 1
 
 char **my_str_to_word_array(char const *str, char c);
+
 char *epur_str(char *str);
+
 int mid_pipe(char **av, int out, int ac);
 
 int close_pipe(char **av, int out)
 {
     char **arguments = my_str_to_word_array(epur_str(av[1]), (' '));
 
-    if(dup2(out, STDOUT_FILENO) == -1) {
+    if (dup2(out, STDOUT_FILENO) == -1) {
         perror("Fils : erreur lors de la duplication du descripteur ");
         return (84);
     }
-    if(dup2(out, STDERR_FILENO) == -1) {
+    if (dup2(out, STDERR_FILENO) == -1) {
         perror("Fils : erreur lors de la duplication du descripteur ");
         return (84);
     }
-    if(close(out) == -1) {
+    if (close(out) == -1) {
         perror("Fils : erreur lors de la fermeture du tube en lecture ");
         return (84);
     }
-    if(execve(arguments[0], arguments, NULL) == -1)
+    if (execve(arguments[0], arguments, NULL) == -1)
         perror("Fils : erreur lors de l'execution de ls ");
     return (84);
 }
@@ -45,46 +47,46 @@ int mid_pipe(char **av, int out, int ac)
     int tube[2];
     pid_t pid;
 
-    if(pipe(tube) == -1) {
+    if (pipe(tube) == -1) {
         perror("Pere : erreur lors de la creation du tube ");
         return (84);
     }
-    if((pid = fork()) == -1) {
+    if ((pid = fork()) == -1) {
         perror("Pere : erreur lors de la creation du fils ");
         return (84);
     }
-    if(pid == 0) {
-        if(close(tube[TUBE_LECTURE]) == -1) {
+    if (pid == 0) {
+        if (close(tube[TUBE_LECTURE]) == -1) {
             perror("Fils : erreur lors de la fermeture du tube en lecture ");
             return (84);
-	}
+    }
         close_pipe(av, tube[TUBE_ECRITURE]);
     }
-    if(close(tube[TUBE_ECRITURE]) == -1) {
-	perror("Pere : erreur lors de la fermeture du tube en ecriture ");
-	exit(EXIT_FAILURE);
+    if (close(tube[TUBE_ECRITURE]) == -1) {
+        perror("Pere : erreur lors de la fermeture du tube en ecriture ");
+        exit(EXIT_FAILURE);
     }
-    if(dup2(tube[TUBE_LECTURE], STDIN_FILENO) == -1) {
-	perror("Pere : erreur lors de la duplication du descripteur ");
-	exit(EXIT_FAILURE);
+    if (dup2(tube[TUBE_LECTURE], STDIN_FILENO) == -1) {
+        perror("Pere : erreur lors de la duplication du descripteur ");
+        exit(EXIT_FAILURE);
     }
-    if(close(tube[TUBE_LECTURE]) == -1) {
-	perror("Pere : erreur lors de la fermeture du tube en lecture ");
-	exit(EXIT_FAILURE);
+    if (close(tube[TUBE_LECTURE]) == -1) {
+        perror("Pere : erreur lors de la fermeture du tube en lecture ");
+        exit(EXIT_FAILURE);
     }
-    if(dup2(out, STDOUT_FILENO) == -1) {
+    if (dup2(out, STDOUT_FILENO) == -1) {
         perror("Fils : erreur lors de la duplication du descripteur ");
         return (84);
     }
-    if(dup2(out, STDERR_FILENO) == -1) {
+    if (dup2(out, STDERR_FILENO) == -1) {
         perror("Fils : erreur lors de la duplication du descripteur ");
         return (84);
     }
-    if(close(out) == -1) {
+    if (close(out) == -1) {
         perror("Fils : erreur lors de la fermeture du tube en lecture ");
         return (84);
     }
-    if(execve(arguments[0], arguments, NULL) == -1)
+    if (execve(arguments[0], arguments, NULL) == -1)
         perror("Fils : erreur lors de l'execution de ls ");
     return (84);
 }
@@ -95,16 +97,16 @@ int create_pipe(char **av, int ac)
     pid_t pid;
     char **arguments = my_str_to_word_array(epur_str(av[ac]), (' '));
 
-    if(pipe(tube) == -1) {
-	perror("Pere : erreur lors de la creation du tube ");
+    if (pipe(tube) == -1) {
+        perror("Pere : erreur lors de la creation du tube ");
         return (84);
     }
-    if((pid = fork()) == -1) {
+    if ((pid = fork()) == -1) {
         perror("Pere : erreur lors de la creation du fils ");
-	return (84);
+        return (84);
     }
-    if(pid == 0) {
-        if(close(tube[TUBE_LECTURE]) == -1) {
+    if (pid == 0) {
+        if (close(tube[TUBE_LECTURE]) == -1) {
             perror("Fils : erreur lors de la fermeture du tube en lecture ");
             return (84);
         }
@@ -113,19 +115,19 @@ int create_pipe(char **av, int ac)
             mid_pipe(av, tube[TUBE_ECRITURE], ac);
         }
     }
-    if(close(tube[TUBE_ECRITURE]) == -1) {
-	perror("Pere : erreur lors de la fermeture du tube en ecriture ");
+    if (close(tube[TUBE_ECRITURE]) == -1) {
+        perror("Pere : erreur lors de la fermeture du tube en ecriture ");
         exit(EXIT_FAILURE);
     }
-    if(dup2(tube[TUBE_LECTURE], STDIN_FILENO) == -1) {
-	perror("Pere : erreur lors de la duplication du descripteur ");
+    if (dup2(tube[TUBE_LECTURE], STDIN_FILENO) == -1) {
+        perror("Pere : erreur lors de la duplication du descripteur ");
         exit(EXIT_FAILURE);
     }
-    if(close(tube[TUBE_LECTURE]) == -1) {
+    if (close(tube[TUBE_LECTURE]) == -1) {
         perror("Pere : erreur lors de la fermeture du tube en lecture ");
-	exit(EXIT_FAILURE);
+        exit(EXIT_FAILURE);
     }
-    if(execve(arguments[0], arguments, NULL) == -1) {
+    if (execve(arguments[0], arguments, NULL) == -1) {
         perror("Pere : erreur lors de l'execution de wc ");
     }
 }
