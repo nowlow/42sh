0878d0ce6be12cfd11c1683b5abdd4d1f1a97756
axo <axo@localhost.localdomain>
2019-05-26 16:05:45 +0200

    added: redirection

--- src/execution/redirection.c
+++ src/execution/redirection.c
@@ -20,8 +20,10 @@ void init_redirection(s_element *node)
     int fd;
     int a;
 
-    for (a = 0; node->data.command->redirections[a]; a++);
-    fd = open(node->data.command->redirections[a - 1],
-    O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR | S_IRGRP | S_IWGRP | S_IWUSR);
+    for (a = 0; node->data.command->redirections[a]; a++){
+        fd = open(node->data.command->redirections[a],
+        O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR | S_IRGRP | S_IWGRP | S_IWUSR);
+    }
     dup2(fd, 1);
+    close(fd);
 }
