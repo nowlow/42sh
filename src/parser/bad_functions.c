94e49a205b1368adce99f4c55045a61e17cb620a
Bosquillon de Jenlis Alexis <balexis@localhost.localhost>
2019-05-26 14:54:04 +0200

    added: other operators

--- src/parser/bad_functions.c
+++ src/parser/bad_functions.c
@@ -5,12 +5,23 @@
 ** bad_function
 */
 
+#include <stdlib.h>
+#include <stdio.h>
 #include "parser/parser.h"
 
 s_element *parse_ok(char *line, char separator)
 {
     char **parts = line_to_parts(line, separator);
+    int n_part;
 
+    for (n_part = 0; parts[n_part]; n_part++);
+    if (n_part <= 1) {
+        dprintf(2, "Invalid null command. %s\n", parts[0]);
+        return NULL;
+    }
+    for (int i = 0; parts[i]; i++) {
+        printf("%d: %s\n", i, parts[i]);
+    }
     return parts_to_elem(parts);
 }
 
--- src/parser/parse.c
+++ src/parser/parse.c
@@ -10,21 +10,7 @@
 #include "parser/parser.h"
 #include "parser/tab.h"
 
-s_element *parts_to_elem(char **parts)
-{
-    int n_part;
-
-    for (n_part = 0; parts[n_part]; n_part++);
-    if (n_part == 0)
-        return NULL;
-    if (n_part == 1)
-        return parse(parts[0]);
-    if (!(strcmp(parts[1], ";") ^ strcmp(parts[1], "|")) || n_part < 3)
-        return NULL;
-    return to_operator_elem(parts);
-}
-
-char get_separator(char *line)
+int check_separator(char *line, char separator)
 {
     int depth = 0;
     int in_marks = 0;
@@ -36,22 +22,36 @@ char get_separator(char *line)
             depth--;
         if (line[i] == '"')
             in_marks = !in_marks;
-        if (depth == 0 && !in_marks && line[i] == ';')
-            return ';';
-    }
-    depth = 0;
-    in_marks = 0;
-    for (int i = 0; line[i]; i++) {
-        if (line[i] == '(')
-            depth++;
-        if (line[i] == ')')
-            depth--;
-        if (line[i] == '"')
-            in_marks = !in_marks;
-        if (depth == 0 && !in_marks && line[i] == '|')
-            return '|';
+        if (depth == 0 && !in_marks && line[i] == separator)
+            return 1;
     }
     return 0;
+}
+
+char get_separator(char *line)
+{
+    char *separator_list = "&;|";
+
+    for (int i = 0; i < separator_list[i]; i++) {
+        if (check_separator(line, separator_list[i]))
+            return separator_list[i];
+    }
+    return 0;
+}
+
+s_element *parts_to_elem(char **parts)
+{
+    int n_part;
+
+    for (n_part = 0; parts[n_part]; n_part++);
+    if (n_part == 0)
+        return NULL;
+    if (n_part == 1) {
+        return parse(parts[0]);
+    }
+    if (!(strcmp(parts[1], ";") ^ strcmp(parts[1], "|")) || n_part < 3)
+        return NULL;
+    return to_operator_elem(parts);
 }
 
 char *remove_parenthesis(char *line)
